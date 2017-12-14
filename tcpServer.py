92320ff7f4605c500c3118ab14dcc034b0ada762
YuzhouPeng <760783896@qq.com>
2017-12-14 21:35:55 +0000

    Add files via upload

--- tcpServer.py
+++ tcpServer.py
@@ -8,28 +8,28 @@ import sys
 
 
 class TCPServer(object):
-    PORT = 8000
-    HOST = '0.0.0.0'
-    LENGTH = 4096
-    MAX_THREAD = 10
-    HELO_RESPONSE = "HELO %s\nIP:%s\nPort:%s\nStudentID:11347076"
-    DEFAULT_RESPONSE = "ERROR: INVALID MESSAGE\n\n"
-    HELO_REGEX = "HELO .*"
+    Port_num = 8000
+    Host_num = '0.0.0.0'
+    Max_length = 4096
+    Max_thread = 10
+    Helo_rep = "HELO %s\nIP:%s\nPort:%s\nStudentID:11347076"
+    Default_rep = "ERROR: INVALID MESSAGE\n\n"
+    Helo_info = "HELO .*"
 
     def __init__(self, port_use=None, handler=None):
         if not port_use:
-            port_use = self.PORT
+            port_use = self.Port_num
         else:
-            self.PORT = port_use
+            self.Port_num = port_use
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.HOST, port_use))
+        self.sock.bind((self.Host_num, port_use))
         self.handler = handler if handler else self.default_handler
         # Create a queue of tasks with ma
-        self.threadQueue = Queue.Queue(maxsize=self.MAX_THREAD)
+        self.threadQueue = Queue.Queue(maxsize=self.Max_thread)
 
         # Create thread pool
-        for i in range(self.MAX_THREAD):
-            thread = ThreadHandler(self.threadQueue, self.LENGTH, self)
+        for i in range(self.Max_thread):
+            thread = ThreadHandler(self.threadQueue, self.Max_length, self)
             thread.setDaemon(True)
             thread.start()
 
@@ -41,14 +41,13 @@ class TCPServer(object):
 
         # Listen for connections and delegate to threads
         while True:
-            con, addr = self.sock.accept()
-
+            con_listen, addr_listen = self.sock.accept()
             # If queue full close connection, otherwise send to thread
             if not self.threadQueue.full():
-                self.threadQueue.put((con, addr))
+                self.threadQueue.put((con_listen, addr_listen))
             else:
-                print "Queue full closing connection from %s:%s" % (addr[0], addr[1])
-                con.close()
+                print "Queue full closing connection from %s:%s" % (addr_listen[0], addr_listen[1])
+                con_listen.close()
 
     def kill_serv(self, con):
         # Kill server
@@ -57,37 +56,35 @@ class TCPServer(object):
 
     def helo(self, con, addr, text):
         # Reply to helo request
-        reply = text.rstrip()  # Remove newline
-        reply = reply.split()[1]
-        return_string = self.HELO_RESPONSE % (reply, addr[0], addr[1])
-        con.sendall(return_string)
+        reply_h = text.rstrip()  # Remove newline
+        reply_h = reply_h.split()[1]
+        return_string_info = self.Helo_rep % (reply_h, addr[0], addr[1])
+        con.sendall(return_string_info)
         return
 
     def default(self, con, addr, text):
-        return_string = self.DEFAULT_RESPONSE
-        con.sendall(return_string)
+        return_string_info = self.Default_rep
+        con.sendall(return_string_info)
         # Default handler for everything else
         print "Default"
         return
 
     def send_request(self, data, server, port):
-        return_data = ""
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        sock.connect((server, port))
-        sock.sendall(data)
-
+        return_data_req = ""
+        sock_info = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_info.connect((server, port))
+        sock_info.sendall(data)
         # Loop until all data received
-        while "\n\n" not in return_data:
-            data = sock.recv(self.LENGTH)
+        while "\n\n" not in return_data_req:
+            data = sock_info.recv(self.Max_length)
             if len(data) == 0:
                 break
-            return_data += data
+            return_data_req += data
 
         # Close and dereference the socket
-        sock.close()
-        sock = None
-        return return_data
+        sock_info.close()
+        sock_info = None
+        return return_data_req
 
 
 class ThreadHandler(threading.Thread):
@@ -101,8 +98,8 @@ class ThreadHandler(threading.Thread):
     def run(self):
         # Thread loops and waits for connections to be added to the queue
         while True:
-            request = self.queue.get()
-            self.handler(request)
+            request_info = self.queue.get()
+            self.handler(request_info)
             self.queue.task_done()
 
     def handler(self, (con, addr)):
@@ -127,17 +124,15 @@ class ThreadHandler(threading.Thread):
                 self.server.default(con, addr, message)
         return
 
-
 def main():
     try:
         if len(sys.argv) > 1 and sys.argv[1].isdigit():
-            port = int(sys.argv[1])
-            server = TCPServer(port)
+            port_num = int(sys.argv[1])
+            server_info = TCPServer(port_num)
         else:
-            server = TCPServer()
-        server.listen()
+            server_info = TCPServer()
+        server_info.listen()
     except socket.error, msg:
         print "Unable to create socket connection: " + str(msg)
-
 
 if __name__ == "__main__": main()
